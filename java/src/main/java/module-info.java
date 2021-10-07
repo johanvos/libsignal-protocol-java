115520f7481cf4836a4adc56537f17e96ad4d1d9
Johan Vos <johan@lodgon.com>
2021-10-07 15:33:54 +0200

    add module-info

---
+++ java/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.whispersystems.protocol {
+    requires com.google.protobuf;
+    requires org.whispersystems.curve25519;
+    exports org.whispersystems.libsignal;
+    exports org.whispersystems.libsignal.ecc;
+    exports org.whispersystems.libsignal.groups.state;
+    exports org.whispersystems.libsignal.kdf;
+    exports org.whispersystems.libsignal.logging;
+    exports org.whispersystems.libsignal.protocol;
+    exports org.whispersystems.libsignal.state;
+    exports org.whispersystems.libsignal.util;
+}
